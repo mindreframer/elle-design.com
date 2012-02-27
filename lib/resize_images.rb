45844952d13765fd6e749d54621c1f136e777494
Roman Heinrich <roman.heinrich@gmail.com>
2012-02-27 18:32:58 +0100

    script slightly improved

--- lib/resize_images.rb
+++ lib/resize_images.rb
@@ -1,10 +1,23 @@
 #!/usr/bin/env ruby
 
 require 'dragonfly'
-originals = Dir['original/*']
+originals = Dir['portfolio/*/**']
 
 app = Dragonfly[:images].configure_with(:imagemagick)
 path = File.expand_path(File.dirname(__FILE__))
+
+
+
+folder_name = 'resized'
+Dir.mkdir(folder_name) unless File.exists?(folder_name)
+originals.each do |f|
+  file_name_part = f.split('/')[1..-1].join('/') # only the last to path parts
+  img = app.fetch_file(File.expand_path(f))
+  img.thumb('1024x640').to_file("#{folder_name}/#{file_name_part}")
+end
+
+# .encode(:jpg, '-quality 80')
+
 ###### Install smusher gem and optimize the folders by hand.
 
 
@@ -20,12 +33,5 @@ path = File.expand_path(File.dirname(__FILE__))
 #   file_name = File.basename(f)
 #   img = app.fetch_file("#{path}/#{f}")
 #   img.thumb('450x450').encode(:jpg, '-quality 80').to_file("my_middle/#{file_name}")
-# end
-#
-# Dir.mkdir('my_big') unless File.exists?('my_big')
-# originals.each do |f|
-#   file_name = File.basename(f)
-#   img = app.fetch_file("#{path}/#{f}")
-#   img.thumb('800x800').encode(:jpg, '-quality 80').to_file("my_big/#{file_name}")
 # end
 #
